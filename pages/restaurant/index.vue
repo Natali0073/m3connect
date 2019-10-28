8e452695d26a40324080ad7813559e979001d170
Mykola <mv@leobit.co>
2019-10-28 08:26:00 +0200

    add restaurant page

--- pages/restaurant/index.vue
+++ pages/restaurant/index.vue
@@ -1,13 +1,31 @@
 <template>
-
+  <div class="d-flex">
+    <div class="side-bar">
+      <Logo/>
+      cont 1
+    </div>
+    <div class="main-bar">
+      cont 2
+    </div>
+  </div>
 </template>
 
 <script>
+  import Logo from '@/components/Logo';
   export default {
-    name: 'index'
+    components: {
+      Logo
+    },
   }
 </script>
 
-<style scoped>
+<style>
+  .side-bar {
+    width: 30%;
+  }
 
+  .main-bar {
+    width: 70%;
+    background: black;
+  }
 </style>
